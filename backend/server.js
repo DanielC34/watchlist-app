e8ccabcee41cef2e32e0d611b2b9b114d42850bc
Daniel Chanda <danielchanda41@gmail.com>
2024-07-20 14:55:10 +0300

    More updates on backend

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,9 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
 
 // Database Connection
 mongoose
@@ -21,10 +21,10 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-// Routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/user", require("./routes/user"));
-app.use("/api/watchlist", require("./routes/watchlist"));
+// // Routes
+// app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/user", require("./routes/user"));
+// app.use("/api/watchlist", require("./routes/watchlist"));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
